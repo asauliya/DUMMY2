243c73d99e572eb278df65dc08cf551c4ef8febd
Anshuman Asauliya <90451238+asauliya@users.noreply.github.com>
2024-10-19 22:04:33 +0530

    Add Bash script

---
+++ script.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# Define the name of the file
+FILENAME="myfile.txt"
+
+# Create the file
+touch $FILENAME
+
+# Optionally, add some content to the file
+echo "This is a new text file." > $FILENAME
+
